a77a6c8a3c8e576e1b449144ad58702fb8a9f269
Slavomír Švigar <49694659+Slavoo97@users.noreply.github.com>
2023-11-25 20:26:57 +0100

    update

--- BE/app/Controllers/LoginController.php
+++ BE/app/Controllers/LoginController.php
@@ -10,6 +10,7 @@ use Contributte\ApiRouter\ApiRoute;
 use Nette\Application\Request;
 use Nette\Application\Response;
 use Nette\Application\Responses\JsonResponse;
+use Nette\Security\Passwords;
 
 /**
  * API for logging users in
@@ -19,7 +20,7 @@ use Nette\Application\Responses\JsonResponse;
  * 	methods={
  * 		"POST"="run"
  * 	},
- *  presenter="Sign",
+ *  presenter="Login",
  *  format="json"
  * )
  */
@@ -34,187 +35,41 @@ final class LoginController extends AbstractController
 
 	public function run(Request $request): Response
 	{
-		return new JsonResponse($this->apiResponseFormatter->formatMessage('Hello'));
-	}
+        $this->authenticator = new Authenticator($this->employeeRepository);
+        $data = $this->getRequestData();
+        $values = json_decode($data);
 
-    public function up(Request $request): Response
-    {
         try {
-            $this->authenticator = new Authenticator($this->employeeRepository);
-            $data = $this->getHttpRequest()->getRawBody();
-            $values = json_decode($data);
-            $error = [];
-            if (!filter_var($values->email, FILTER_VALIDATE_EMAIL)) {
-                $error[] = [
-                    'key' => 'email',
-                    'code' => 'messages.validation.email.invalid'
-                ];
-                $this->resource->error = $error;
-                $this->resource->success = false;
+            $user = $this->employeeRepository->findEmployeeBy(['email' => $values->email]);
+            if ($user === null) {
+                return new JsonResponse($this->apiResponseFormatter->formatError("404", "user is null"));
+            } else if ($user->isEnabled() !== true) {
+                return new JsonResponse($this->apiResponseFormatter->formatError("404", "user is not enabled"));
             } else {
-                $language = $this->grammarian->getDefaultLanguage();
-                $customerExist = $this->employeeRepository->countOfCustomers(['email' => $values->email, 'weakCustomer' => false]);
-                $error = [];
-                if ($customerExist == 1) {
-                    $error[] = [
-                        'key' => 'email',
-                        'code' => 'messages.validation.email.exists'
-                    ];
-                    $this->resource->error = $error;
-                    $this->resource->success = false;
+                if ($values->password != $user->getPassword()) {
+                    return new JsonResponse($this->apiResponseFormatter->formatError("404", "bad password"));
                 } else {
-                    $error = $this->employeeRepository->validateNewPassword($values->password, true);
-                    if ($error !== null) {
-                        $this->resource->error = [$error];
-                        $this->resource->success = false;
-                    } else {
-                        $validate = Validate::require($values, [
-                            'email',
-                            'password',
-                            'name',
-                            'surname',
-                            'phone',
-                            'newsletter',
-                            'street',
-                            'zip',
-                            'city',
-                            'state',
-                        ]);
-                        if ($validate['success'] === false) {
-                            $this->resource->success = false;
-                            $this->resource->error = $validate['error'];
-                        } else {
-                            $checkWeekCustomer = $this->employeeRepository->findCustomerBy(['email' => $values->email, 'weakCustomer' => true]);
 
-                            // priradenie do skupiny "fleX"
-                            $customerGroupsIds = [];
-                            $enabledAfterRegistration = true;
-                            if (isset($values->industry)) {
-                                $customerGroupsIds[] = CustomerGroupProvider::GROUP_ID_FLEX;
-                                $enabledAfterRegistration = false; // po zaregistrovani do fleX, je ucet neaktivny, kym nie je aktivovany obchodakom
-                            } else {
-                                $customerGroupsIds[] = CustomerGroupProvider::GROUP_ID_B2B;
-                            }
+                    $this->employeeRepository->setLastLogin($user);
 
-
-                            if (isset($checkWeekCustomer)) {
-                                $customer = $this->employeeRepository->changeCustomer(
-                                    $checkWeekCustomer->getId(),
-                                    isset($values->companyName) ? $values->companyName : "",
-                                    $values->name,
-                                    $values->surname,
-                                    $values->phone,
-                                    $values->email,
-                                    $values->password,
-                                    $values->newsletter,
-                                    null,
-                                    null,
-                                    $language->getId(),
-                                    false,
-                                    $enabledAfterRegistration,
-                                    $customerGroupsIds,
-                                    false,
-                                    isset($values->companyId) ? $values->companyId : null,
-                                    isset($values->vatRegistrationId) ? $values->vatRegistrationId : null,
-                                    isset($values->extVatRegistrationId) ? $values->extVatRegistrationId : null,
-                                    isset($values->industry) ? $values->industry : null,
-                                    false,
-                                    '',
-                                    true
-                                );
-                            } else {
-                                $customer = $this->employeeRepository->createCustomer(
-                                    isset($values->companyName) ? $values->companyName : "",
-                                    $values->name,
-                                    $values->surname,
-                                    $values->phone,
-                                    $values->email,
-                                    $values->password,
-                                    $values->newsletter,
-                                    $language->getId(),
-                                    false,
-                                    $enabledAfterRegistration,
-                                    $customerGroupsIds,
-                                    false,
-                                    isset($values->companyId) ? $values->companyId : null,
-                                    isset($values->vatRegistrationId) ? $values->vatRegistrationId : null,
-                                    isset($values->extVatRegistrationId) ? $values->extVatRegistrationId : null,
-                                    isset($values->industry) ? $values->industry : null,
-                                    false,
-                                    null
-                                );
-                            }
-
-                            $this->customerAddressProvider->createCustomerAddress(
-                                null, //array_key_exists("companyName", $values) ? $values->companyName : null,
-                                $values->name,
-                                $values->surname,
-                                $values->street,
-                                $values->zip,
-                                $values->city,
-                                $values->state,
-                                CustomerAddress::INVOICE_ADDRESS,
-                                $values->phone,
-                                null,
-                                null,
-                                $customer
-                            );
-
-                            $this->customerAddressProvider->createCustomerAddress(
-                                null,
-                                $values->name,
-                                $values->surname,
-                                $values->street,
-                                $values->zip,
-                                $values->city,
-                                $values->state,
-                                CustomerAddress::DELIVERY_ADDRESS,
-                                $values->phone,
-                                null,
-                                null,
-                                $customer
-                            );
-
-                            $this->employeeRepository->sendConfirmRegistrationEmail($customer, $language);
-
-                            $user = null;
-                            if ($enabledAfterRegistration) {
-                                //login after successful registration
-
-                                $this->getUser()->setAuthenticator($this->authenticator);
-                                $this->getUser()->login($customer->getEmail(), $values->password, false);
-                                $this->getUser()->setExpiration('24 hours', false);
-
-
-                                $authCheck = (new DateTime())->getTimestamp();
-                                $this->employeeRepository->setLastLogin($customer);
-                                $this->employeeRepository->setAuthCheck($customer, $authCheck);
-                                $userId = $customer->getId();
-
-                                $user = [
-                                    'id'         => Utils::encrypt($userId),
-                                    'username'   => $customer->getEmail(),
-                                    'first_name' => $customer->getFirstName(),
-                                    'last_name'  => $customer->getLastName(),
-                                    'flex_group' => $this->employeeRepository->isInFlexGroup($userId),
-                                    "auth_check" => $authCheck
-                                ];
-                            }
-
-                            $this->resource->success = true;
-                            $this->resource->user = $user;
-                        }
-                    }
+                    $userInfo = [
+                        'id'         => $user->getId(),
+                        'email'   => $user->getEmail(),
+                        'first_name' => $user->getName(),
+                        'surname'  => $user->getSurname(),
+                        'phone'      => $user->getMobileNumber()
+                    ];
+                    return new JsonResponse($this->apiResponseFormatter->formatPayload($userInfo));
                 }
             }
         } catch (\Exception $e) {
+            $this->resource->user = null;
             $this->resource->success = false;
             $this->resource->error = [
                 'key' => 'server',
                 'code' => $e->getMessage()
             ];
         }
-
-        return new JsonResponse($this->apiResponseFormatter->formatMessage('Hello'));
-    }
+		return new JsonResponse($this->apiResponseFormatter->formatMessage('Hello'));
+	}
 }
