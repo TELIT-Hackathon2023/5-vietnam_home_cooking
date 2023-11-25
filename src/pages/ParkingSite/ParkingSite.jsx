da0b2b9cd4d274302d4e37e55333e78216238934
Alex Vereš <alex.veres@ibm.com>
2023-11-26 00:21:20 +0100

    parking updates

--- src/pages/ParkingSite/ParkingSite.jsx
+++ src/pages/ParkingSite/ParkingSite.jsx
@@ -1,79 +1,31 @@
 import React, { useState } from 'react';
-import Parking from '../../components/Parking/Parking.jsx';
-import { Box, VStack, Text, Stack, Heading, Grid } from '@chakra-ui/react';
-import TParking from '../../assets/tparking.svg';
-import { Image } from '@chakra-ui/react';
-import { Button } from '@chakra-ui/react';
+import './ParkingSite.css';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
+import NavBar from '../../components/NavBar/NavBar.jsx';
+import Dashboard from '../../components/Dashboard/Dashboard.jsx';
 
 const ParkingSite = () => {
+  const [currentPage, setCurrentPage] = useState('dashboard');
+
+  const handleNavItemClick = (page) => {
+    setCurrentPage(page);
+  };
+
   return (
-    <Box>
-      <Stack direction='row' className='page'>
-        {/* Navbar on the left */}
-        <Box
-          position='absolute'
-          height='100vh'
-          width='12rem'
-          bg='white'
-          color='#2e2e2e'
-          borderRight='3px solid #f2007c'
-        >
-          <VStack
-            spacing='5'
-            alignItems='space between'
-            paddingLeft={'6'}
-            paddingRight={'6'}
-            justifyContent={'center'}
-            paddingTop='8'
-          >
-            <Image src={TParking} boxSize='100px' marginBottom />
-            <Button
-              className='buttons'
-              bg='#f2007c'
-              variant='solid'
-              size='md'
-              color='#FFFFFF'
-              _hover={{ backgroundColor: '#8a094b' }}
-            >
-              Dashboard
-            </Button>
-            <Text>Map</Text>
-            <Text>My bookings</Text>
-            <Text align='left'>Waiting list</Text>
-          </VStack>
-        </Box>
+    <div className='content-container'>
+      <Box bg='white' w='70%' h='75%' borderRadius='2rem' overflow='hidden'>
+        <Flex direction='row' height='100%'>
+          {/* Vertical Navbar */}
+          <NavBar onNavItemClick={handleNavItemClick} />
 
-        {/* Main Content */}
-        <Box marginLeft='12rem' width='100%' height='100vh'>
-          <Grid templateColumns='repeat(2, 1fr)' gap={8} p={7} height='100vh'>
-            {/* Two big boxes stacked vertically */}
-            <VStack spacing={6}>
-              <Box border='3px solid #2e2e2e' height='40%' width='100%' borderRadius='8px' p={6}>
-                <Heading size='md'>Currently booked</Heading>
-              </Box>
-              <Box border='3px solid #2e2e2e' height='60%' width='100%' borderRadius='8px' p={6}>
-                <Heading as={'h1'} size='md'>
-                  Telecom parking slots
-                </Heading>
-              </Box>
-            </VStack>
-
-            {/* Three smaller boxes stacked vertically */}
-            <VStack spacing={6} alignItems='flex-start'>
-              <Box border='3px solid #2e2e2e' height='20%' width='100%' borderRadius='8px' p={6}>
-                <Heading size='md'>Your vehicles</Heading>
-              </Box>
-              <Box border='3px solid #2e2e2e' height='30%' width='100%' borderRadius='8px' p={6}>
-                <Heading size='md'>Calendar</Heading>
-              </Box>
-              <Box border='3px solid #2e2e2e' height='50%' width='100%' borderRadius='8px' p={6}>
-                <Heading size='md'>Waiting list</Heading>
-              </Box>
-            </VStack>
-          </Grid>
-        </Box>
-      </Stack>
-    </Box>
+          {/* Dashboard */}
+          <Box flex='1' p='4'>
+            {currentPage === 'dashboard' && <Dashboard />}
+            {/* Add other pages as needed */}
+          </Box>
+        </Flex>
+      </Box>
+    </div>
   );
 };
 
