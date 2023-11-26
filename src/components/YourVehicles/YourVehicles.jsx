5377883fe930035d8eec28f23d9025de10daa60d
Eduard Ridilla <eduard.ridilla@softacus.sk>
2023-11-26 06:27:02 +0100

    stuff

--- src/components/YourVehicles/YourVehicles.jsx
+++ src/components/YourVehicles/YourVehicles.jsx
@@ -1,25 +1,35 @@
 import React, { useEffect } from 'react';
-import { Table, Tbody, Td, Tr } from '@chakra-ui/react';
+import { Box, HStack, Image, Table, Tbody, Td, Tr, Text } from '@chakra-ui/react';
 import { useUserContext } from '../../hooks/UserContext.jsx';
 
 const YourVehicles = () => {
   const { userData } = useUserContext();
 
   return (
-    <Table variant='simple' colorScheme='whiteAlpha'>
-      <Tbody>
-        {userData.vehicles.map((vehicle) => (
-          <Tr>
-            <Td borderBottom='1px solid' borderColor='gray.200'>
-              {vehicle.carBrand}
-            </Td>
-            <Td borderBottom='1px solid' borderColor='gray.200'>
-              {vehicle.plateNumber}
-            </Td>
-          </Tr>
-        ))}
-      </Tbody>
-    </Table>
+    <>
+      <Table variant='simple' colorScheme='whiteAlpha'>
+        <Tbody>
+          {userData.vehicles.map((vehicle) => {
+            const imgPath = '/src/assets/brands/' + vehicle.carBrand + '.png';
+            console.log(imgPath);
+
+            return (
+              <Tr>
+                <Td borderBottom='1px solid' borderColor='gray.200'>
+                  <HStack>
+                    <Image src={imgPath} w='2rem' h='2rem' />
+                    <Text>{vehicle.carBrand}</Text>
+                  </HStack>
+                </Td>
+                <Td borderBottom='1px solid' borderColor='gray.200'>
+                  {vehicle.plateNumber}
+                </Td>
+              </Tr>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </>
   );
 };
 
